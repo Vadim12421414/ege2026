9b6bb38815eb2f9137fe1b6acf129eaabf383abd
Vadim <vadimtsurikov2008@mail.ru>
2026-04-24 19:26:00 +0200

    3-й тип 27 задачи

--- task_27/17834_b.py
+++ task_27/17834_b.py
@@ -1,6 +1,6 @@
 from math import *
 
-with open(r'27_B_17834.txt') as file:
+with open(r'../27/27_B_17834.txt') as file:
     dots = [list(map(float, i.replace(',', '.').split())) for i in file]
 
 
--- task_27/17915_a.py
+++ task_27/17915_a.py
@@ -1,6 +1,6 @@
 from math import *
 
-with open(r'27_B_17915.txt') as file:
+with open(r'../27/27_B_17915.txt') as file:
     dots = [list(map(float, i.replace(',', '.').split())) for i in file]
 
 def center(cluster):
